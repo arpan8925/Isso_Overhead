d181af7f0d4ae8b55a959b1279ae11b8a00fcd7f
arpan8925 <sbxp1966@gmail.com>
2024-06-19 02:41:44 +0530

    Sunset_Sunrise Printing Test

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 from datetime import datetime
 
-MY_LAT = 20 # Your latitude
-MY_LONG = 40 # Your longitude
+MY_LAT = 23.037451 # Your latitude
+MY_LONG = 72.563980 # Your longitude
 
 def postion_check():  
     if iss_latitude >= MY_LAT-5 and iss_latitude <= MY_LAT+5 and iss_longitude >= MY_LONG-5 and iss_longitude <= MY_LONG+5:
@@ -31,7 +31,18 @@ data = response.json()
 sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
 sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
+
 time_now = datetime.now()
+hour_now = time_now.hour
+
+
+
+if sunset <= hour_now and hour_now >= sunrise:
+    print("Raat Bhai")
+else:
+    print("Din Bhai")
+
+
 
 # If the ISS is close to my current position
 # and it is currently dark
