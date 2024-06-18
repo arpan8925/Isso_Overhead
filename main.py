d67f9cec404b1edb486aee2f37c017bfac4f2224
arpan8925 <sbxp1966@gmail.com>
2024-06-19 02:22:21 +0530

    ISS Position Is near me Checked

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import requests
 from datetime import datetime
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+MY_LAT = 20 # Your latitude
+MY_LONG = 40 # Your longitude
+
+def postion_check():  
+    if iss_latitude >= MY_LAT-5 and iss_latitude <= MY_LAT+5 and iss_longitude >= MY_LONG-5 and iss_longitude <= MY_LONG+5:
+        return True
+    else:
+        return False
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -11,8 +17,7 @@ data = response.json()
 iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
 
-#Your position is within +5 or -5 degrees of the ISS position.
-
+postion_check()
 
 parameters = {
     "lat": MY_LAT,
@@ -28,7 +33,7 @@ sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
 time_now = datetime.now()
 
-#If the ISS is close to my current position
+# If the ISS is close to my current position
 # and it is currently dark
 # Then send me an email to tell me to look up.
 # BONUS: run the code every 60 seconds.
